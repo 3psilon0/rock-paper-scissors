036a08e403d8812c74b199c645c51b74355d738c
3psilon0 <bestofluck110@gmail.com>
2024-08-20 12:34:22 +0500

    Did some changes

--- index.js
+++ index.js
@@ -1,104 +1,79 @@
+const content = document.getElementById("content");
+const playButtonWrapper = document.getElementsByClassName("play-button-wrapper");
 const playButton = document.getElementById("play-button");
 
-function getPlayerChoice(){
-    const choice = prompt("Enter: \n - Rock\n - Scissors\n - Paper.\n").toLowerCase();
-    return (choice === 'rock' || choice === 'scissors' || choice === 'paper') ? choice : " ";
+
+// Returns 0, 1 or 2
+// 0 - Rock, 1 - Paper, 2 - Scissors.
+function getComputerChoice(){
+    return (Math.floor(Math.random() * 10)%3);
 }
 
-function getComputerChoice(){
-    const choices = ['rock', 'scissors', 'paper'];
-    // Index that is used to return random value from choices
-    randomIndex = Math.floor(Math.random() * 10)%3;
-    return choices[randomIndex];
+
+function playRound(playerChoice, computerChoice){
+    let result = '';
+
+    if(playerChoice == computerChoice){
+        return 'draw';
+    }
+    switch(playerChoice){
+        case 0:
+            result = computerChoice == 1 ? 'loss' : 'victory';
+            break;
+        case 1:
+            result = computerChoice == 2 ? 'loss' : 'victory';
+            break;
+        case 2:
+            result = computerChoice == 0 ? 'loss' : 'victory';
+            break;
+        default:
+            throw new Error('Invalid option!');
+    }
+
+    return result;
 }
 
 playButton.addEventListener("click", () => {
-    let numberOfRounds = Number(prompt("How many rounds do you want to play?"));
+    playButton.innerText = "Quit";
+    playButton.style.cssText = "background-color:red;color:black;border:2px solid black";
+
+    let nRounds = 5;
     let playerScore = 0;
     let computerScore = 0;
+    let computerChoice, playerChoice;
 
-    while(numberOfRounds){
-        const computerChoice = getComputerChoice();
-        const playerChoice = getPlayerChoice();
+    playButton.addEventListener("click", () => {
+        content.textContent = `Your score is: ${playerScore}`;
+        playButton.innerText = "Restart";
+        playButton.addEventListener("click", () => {
+            document.location.reload();
+        })
+    });
 
-        if(playerChoice === computerChoice){
-            console.log("Draw! Nobody gets a point.");
-            numberOfRounds--;
-            continue;
-        }
-        if(playerChoice === "rock"){
-            if(computerChoice === "scissors"){
-                playerScore++;
-                console.log(`
-                    You win!\n
-                    Computer chose ${computerChoice}
-                    ${playerChoice} beats ${computerChoice}\n
-                    Current Score is\n
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-            else{
-                computerScore++;
-                console.log(`
-                    You lose!
-                    Computer chose ${computerChoice}
-                    ${computerChoice} beats ${playerChoice}
-                    Current Score is
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-        }
-        else if(playerChoice === "scissors"){
-            if(computerChoice === "paper"){
-                playerScore++;
-                console.log(`
-                    You win!\n
-                    Computer chose ${computerChoice}
-                    ${playerChoice} beats ${computerChoice}\n
-                    Current Score is\n
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-            else{
-                computerScore++;
-                console.log(`
-                    You lose!
-                    Computer chose ${computerChoice}
-                    ${computerChoice} beats ${playerChoice}
-                    Current Score is
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-        }
-        else if(playerChoice === "paper"){
-            if(computerChoice === "rock"){
-                playerScore++;
-                console.log(`
-                    You win!\n
-                    Computer chose ${computerChoice}
-                    ${playerChoice} beats ${computerChoice}\n
-                    Current Score is\n
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-            else{
-                computerScore++;
-                console.log(`
-                    You lose!
-                    Computer chose ${computerChoice}
-                    ${computerChoice} beats ${playerChoice}
-                    Current Score is
-                    Player Score: ${playerScore}
-                    Computer Score: ${computerScore}`);
-            }
-        }
-        else{
-            alert("Please enter valid choice!");
-            continue;
-        }
-        numberOfRounds--;
-    }
 
-    alert(playerScore > computerScore ? "Congrats! you win." : playerScore === computerScore ? "It is a draw! Nobody wins." : "You lose!");
+
+    const scoreBox = document.createElement("div");
+    scoreBox.style.cssText = "display:flex;justify-content:space-between;"
+    const choiceBox = document.createElement("div");
+    choiceBox.style.cssText = "display:flex;justify-content:space-between";
+    const textBox = document.createElement("div");
+
+
+
+
+
+    const rock = document.createElement("button");
+    rock.innerHTML = `<img src="./assets/images/rock.png" alt="Image of rock">`;
+    const paper = document.createElement("button");
+    paper.innerHTML = `<img src="./assets/images/paper.png" alt="Image of paper">`;
+    const scissors = document.createElement("button");
+    scissors.innerHTML = `<img src="./assets/images/scissors.png" alt="Image of scissors">`;
+    choiceBox.append(
+        rock,
+        paper,
+        scissors
+    );
+
+    content.append(scoreBox, choiceBox, textBox);
 
 })
